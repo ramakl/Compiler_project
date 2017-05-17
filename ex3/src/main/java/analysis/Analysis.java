8eedcb80e43ed969f4a017aa05cc73758c3ab65d
Rama klaho <rama.m.klaho@gmail.com>
2017-05-17 23:10:27 +0200

    pass cycle test

--- ex3/src/main/java/analysis/Analysis.java
+++ ex3/src/main/java/analysis/Analysis.java
@@ -19,28 +19,90 @@ public class Analysis {
 
     public void check() {
         //TODO implement type checking here!
-        MJClassDeclList cdl= prog.getClassDecls();
+        MJClassDeclList cdl = prog.getClassDecls();
         for (int j = 0; j < cdl.size(); j++) {
             MJClassDecl cd = cdl.get(j);
             MJExtended ce = cd.getExtended();
-            boolean find=false;
-            if(!(ce.toString().equals("ExtendsNothing"))) {
+            boolean find = false;
+            if (!(ce.toString().equals("ExtendsNothing"))) {
                 for (int ii = 0; ii < cdl.size(); ii++) {
 
                     if (ce.toString().equals("ExtendsClass(" + cdl.get(ii).getName() + ")")) {
-                       find = true;
+                        find = true;
                     }
 
                 }
-                if (find== false) {
-                    addError(ce,"not exist");
+                if (find == false) {
+                    addError(ce, "not exist");
                 }
 
 
             }
         }
+        //check for cycle
+      /*  List<MJExtended> ex = new LinkedList<>();
+        for (int j = 0; j < cdl.size(); j++) {
+            MJClassDecl cd = cdl.get(j);
+            MJExtended ce = cd.getExtended();
+            if (!(ce.toString().equals("ExtendsNothing"))) {
+                ex.add(ce);
+                int k = 0;
+                do {
+                       for (int h=0;h<ex.size();h++)
+                       {
+                           cx.
+                       }
 
+                    if (ce.equals(ex.get(k))) {
+                        addError(ce, "there is a cycle");
+                    } else ex.add(ce);
+                    k++;
+                } while (k < ex.size());
+
+            }
+        }*/
+        MJClassDeclList cdll = prog.getClassDecls();
+        List<MJExtended> ex = new LinkedList<>();
+        for (int j = 0; j < cdll.size(); j++) {
+            MJClassDecl cdd = cdll.get(j);
+            MJExtended cee = cdd.getExtended();
+
+            //boolean find = false;
+
+
+
+                  if (!(cee.toString().equals("ExtendsNothing"))) {
+                      ex.add(cee);
+                    for (int ii = 0; ii < cdl.size(); ii++) {
+
+                        if (cee.toString().equals("ExtendsClass(" + cdll.get(ii).getName() + ")")) {
+                            //find = true;
+                            cee = cdll.get(ii).getExtended();
+                            for (int kk = 0; kk < ex.size(); kk++)
+                                if (cee.equals(ex.get(kk)))
+                                    addError(cee, "cycle");
+                                else
+                                    ex.add(cee);
+
+                        }
+
+
+
+
+
+                    }
+                   // if (find == false) {
+                     //   addError(ce, "not exist");
+                    //}
+                }
+
+
+
+        }
     }
+
+
+
 
     public List<TypeError> getTypeErrors() {
         return new ArrayList<>(typeErrors);
