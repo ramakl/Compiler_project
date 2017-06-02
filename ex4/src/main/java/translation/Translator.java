3a007726a6ecf9887e2475d31f35c01a927607b9
Rama klaho <rama.m.klaho@gmail.com>
2017-06-02 21:25:16 +0200

    translate plus

--- ex4/src/main/java/translation/Translator.java
+++ ex4/src/main/java/translation/Translator.java
@@ -1,11 +1,12 @@
 package translation;
 
+import analysis.TypeContext;
 import minijava.ast.*;
 import minillvm.ast.*;
 import static minillvm.ast.Ast.*;
 
 
-public class Translator {
+public class Translator extends Element.DefaultVisitor {
 
 	private final MJProgram javaProg;
 
@@ -35,4 +36,55 @@ public class Translator {
 		return prog;
 	}
 
+	@Override
+	public void visit(Prog prog) {
+
+		prog = Prog(TypeStructList(), GlobalList(), ProcList());
+
+		BasicBlockList blocks = BasicBlockList();
+		Proc mainProc = Proc("main", TypeInt(), ParameterList(), blocks);
+		prog.getProcedures().add(mainProc);
+
+
+		BasicBlock entry = BasicBlock(
+				Print(ConstInt(42)),
+				ReturnExpr(ConstInt(0))
+		);
+		entry.setName("entry");
+		blocks.add(entry);
+		super.visit(prog);
+	}
+
+	@Override
+	public void visit(Print print) {
+
+		super.visit(print);
+	}
+	@Override
+	public void visit(Add add) {
+       
+		super.visit(add);
+	}
+	@Override
+	public void visit(InstructionList instructionList) {
+		for (Instruction i : instructionList ) {
+			if(i instanceof MJExprBinary)
+			{
+				MJExpr el= ((MJExprBinary) i).getLeft();
+				MJExpr er=((MJExprBinary) i).getRight();
+				TemporaryVar x=TemporaryVar(el.toString());
+				TemporaryVar y=TemporaryVar(er.toString());
+				MJOperator op=((MJExprBinary) i).getOperator();
+				TemporaryVar R=TemporaryVar(el.toString());
+
+				if(op instanceof MJPlus)
+				{
+					BinaryOperation(R,VarRef(x),Add(),VarRef(y));
+				}
+
+		}
+		}
+	}
+
 }
+
